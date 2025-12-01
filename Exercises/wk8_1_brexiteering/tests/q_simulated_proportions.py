a6820e56a27ebc0c95fe1a6ba5b87fc2e1261bda
Andrew Olson <a.c.olson@bham.ac.uk>
2025-12-01 18:25:45 +0000

    added wk8_1 exercises

---
+++ Exercises/wk8_1_brexiteering/tests/q_simulated_proportions.py
@@ -0,0 +1,65 @@
+
+test = {
+  'name': 'Question simulated_proportions',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # You need to set the value for 'simulated_proportions'
+          >>> 'simulated_proportions' in vars()
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> # You haven't changed the value for 'simulated_proportions'
+          >>> # from its initial state (of ...)
+          >>> simulated_proportions is not ...
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> # simulated_proportions should have 10000 values
+          >>> len(simulated_proportions) == 10000
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> # simulated_proportions values should be proportions
+          >>> assert np.all(np.logical_and(
+          ...     simulated_proportions >= 0,
+          ...     simulated_proportions <= 1))
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          # n = 1000
+          # samps = np.random.binomial
+          'code': r"""
+          >>> # simulated_proportions values should be between 0.44 and 0.6
+          >>> assert np.all(np.logical_and(
+          ...     simulated_proportions >= 0.44,
+          ...     simulated_proportions <= 0.6))
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
